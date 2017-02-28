e83070d7434c26987a37de4146eff3dfec0b5155
unknown <lea.buchweitz@gmx.de>
2017-02-28 17:44:00 +0100

    Initial Commit

---
+++ Farbquiz_Test/Assets/CalculateJumpParab.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateJumpParab : MonoBehaviour {
+
+    private Vector3 quarter;
+    private Vector3 middle;
+    private Vector3 threeQuarter;
+
+    private GameObject cam;
+    private GameObject startJump;
+    private GameObject quarterJump;
+    private GameObject halfJump;
+    private GameObject threeQuarterJump;
+    private GameObject endJump;
+    private GameObject[] canvas;
+
+    private float timer;
+    private bool disappear;
+
+	// Use this for initialization
+	void Start () {
+
+        // Get Reference to camera for FollowSpline()
+        cam = GameObject.Find("CardboardMain");
+
+        // Get references to the EmptyObjects (children) for spline locations
+        startJump = GameObject.Find("1startJump");
+        quarterJump = GameObject.Find("2QuarterJump");
+        halfJump = GameObject.Find("3HalfJump");
+        threeQuarterJump = GameObject.Find("4ThreeQuarterJump");
+        endJump = GameObject.Find("5endJump");
+
+        timer = 0.0f;
+        disappear = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // starts timer to delete question and answers
+        if(disappear)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if(timer > 3.0f)
+        {
+            // gets every component of the question and answers
+            canvas = GameObject.FindGameObjectsWithTag("Canvas");
+            // deletes every question component
+            foreach(GameObject obj in canvas)
+            {
+                Destroy(obj);
+            }
+
+            // reset 
+            disappear = false;
+            timer = 0.0f;
+        }
+	}
+
+    public void calculateLocalParab(Vector3 start, Vector3 end)
+    {
+        //Debug.Log("Start: " + start + " Ende: " + end);
+
+        // Richtungsvektor end - start
+        Vector3 direction = new Vector3(end.x + start.x, end.y - start.y, end.z - start.z);
+        //Debug.Log("Richtungsvektor: " + direction);
+
+        // Mittelpunkt der Animation: start-Vektor + (0.5 * Richtungsvektor) --- max Höhe des Sprungs: y + 1.3
+        middle = new Vector3(start.x + (0.5f * direction.x), start.y + (0.5f * direction.y) + 1.3f, start.z + (0.5f * direction.z));
+        //Debug.Log("Mittelpunkt des Paraboloids: " + middle);
+
+        // Viertel der Animation: start-Vektor + (0.25 * Richtungsvektor) -- halbe Höhe des Sprungs: y + 1.0
+        quarter = new Vector3(start.x + (0.25f * direction.x), start.y + (0.25f * direction.y) +  1f, start.z + (0.25f * direction.z));
+        //Debug.Log("Viertel: " + quarter);
+
+        // Dreiviertel der Animation: start-Vektor + (0.75 * Richtungsvektor) -- halbe Höhe des Sprungs: y + 1.0
+        threeQuarter = new Vector3(start.x + (0.75f * direction.x), start.y + (0.75f * direction.y) + 1f, start.z + (0.75f * direction.z));
+        //Debug.Log("3/4: " + threeQuarter);
+
+        // add calculated locations to the EmptyObjects
+        // start- und end-Höhe des Sprungs auf y + 0.5 (schon in den Parametern)
+        startJump.GetComponent<Transform>().position = start;
+        quarterJump.GetComponent<Transform>().position = quarter;
+        halfJump.GetComponent<Transform>().position = middle;
+        threeQuarterJump.GetComponent<Transform>().position = threeQuarter;
+        endJump.GetComponent<Transform>().position = end;
+
+        // start animation
+        cam.GetComponent<SplineController>().FollowSpline();
+
+        // starts timer for begin to make the question and answers disappear
+        disappear = true;
+    }
+}
---
+++ Farbquiz_Test/Assets/FallingDeep.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class FallingDeep : MonoBehaviour {
+
+    GameObject defaultY;
+    GameObject cam;
+    GameObject[] schollen;
+    GameObject[] canvas;
+    Vector3[] schollenPosition;
+    Vector3 camPosition;
+    Vector3 endJump;
+    float yPosition;
+    float timer;
+    float timerToAnswer;
+    bool correctAnswer;
+    bool jump;
+    string canvasObj;
+    string scholleObj;
+
+	// Use this for initialization
+	void Start () {
+
+        // reference to one Scholle
+        defaultY = GameObject.Find("KameraScholle");
+
+        // gets reference and position to cam
+        cam = GameObject.Find("CardboardMain");
+        camPosition = cam.GetComponent<Transform>().position;
+
+        // sets timer and begin of timer
+        timer = timerToAnswer = 0.0f;
+        correctAnswer = true;
+        jump = false;
+        canvasObj = "Canvas";
+        scholleObj = "Scholle";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // checks y position if Schollen have already fallen deep enough
+        yPosition = defaultY.GetComponent<Transform>().position.y;
+
+        // starts timer for falling __________________________________________ WRONG _________________________________________________________________________________________________________________
+        if (!correctAnswer)
+        {
+            timer += Time.deltaTime;
+        }
+
+        // start falling of all Schollen  _____________________________ FALLING __________________________________________________
+        if (timer > 1.8)
+        {
+            foreach (GameObject objct in schollen)
+            {
+                objct.GetComponent<Rigidbody>().isKinematic = false;
+                GameObject.Find(canvasObj).GetComponentInChildren<Text>().color = Color.red;
+            }
+        }
+
+        // starts cam falling
+        if (timer > 1.95)
+        {
+            cam.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        if (yPosition < -40) //_____________________________________________________ RESET ____________________________________________
+        {
+            // resets positions of Schollen and isKinematic to stop them falling
+            for (int i = 0; i < schollen.Length; i++)
+            {
+                schollen[i].GetComponent<Rigidbody>().isKinematic = true;
+                schollen[i].GetComponent<Transform>().position = schollenPosition[i];
+            }
+
+            // resets cam position and isKinematic to stop it from falling
+            cam.GetComponent<Rigidbody>().isKinematic = true;
+            cam.GetComponent<Transform>().position = camPosition;
+
+            // disables the EventTrigger so that the answer can't be chosen anymore
+            canvas = GameObject.FindGameObjectsWithTag("Canvas");
+            foreach (GameObject obj in canvas)
+            {
+                obj.GetComponent<EventTrigger>().enabled = false;
+            }
+
+            // reset
+            correctAnswer = true;
+            timer = 0.0f;
+            canvasObj = "Canvas";
+        }
+        //________________________________________________________________________________________________________________________________________________________________________________________________
+
+        if (jump) //____________________________________ RIGHT ___________________________________________________________________________________________________________________________________________
+        {
+            timerToAnswer += Time.deltaTime;
+        }
+
+        if (timerToAnswer > 1.8f) //____________________________________ JUMPING _______________________________________
+        {
+            // calls function in CalculateJumpParab to animate jump to correct Scholle
+            cam.GetComponent<CalculateJumpParab>().calculateLocalParab(camPosition, new Vector3(endJump.x, endJump.y + 0.5f, endJump.z));
+
+            // reset 
+            canvasObj = "Canvas";
+            scholleObj = "Scholle";
+            timerToAnswer = 0.0f;
+            jump = false;
+
+            // disables the EventTrigger so that the answer can't be chosen anymore
+            canvas = GameObject.FindGameObjectsWithTag("Canvas");
+            foreach (GameObject obj in canvas)
+            {
+                obj.GetComponent<EventTrigger>().enabled = false;
+            }
+        }
+        //_________________________________________________________________________________________________________________________________________________________________________________________________
+    }
+
+    public void gravityOn(string correct_Name)
+    {
+        // gets all possible Schollen everytime gravityOn is called
+        schollen = GameObject.FindGameObjectsWithTag("Scholle");
+
+        // gets position of all Schollen only if they're not falling
+        if(schollen[0].GetComponent<Rigidbody>().isKinematic)
+        {
+            // gets all positions to all Schollen
+            schollenPosition = new Vector3[schollen.Length];
+            for (int i = 0; i < schollen.Length; i++)
+            {
+                schollenPosition[i] = schollen[i].GetComponent<Transform>().position;
+            }
+        }
+
+        // sets correctness of answer
+        if(correct_Name.StartsWith("1"))
+        {
+            // sets string of current object of Canvas and Scholle
+            canvasObj += correct_Name.ToCharArray().GetValue(1).ToString();
+            scholleObj += correct_Name.ToCharArray().GetValue(1).ToString();
+            // searches current object in Schollen and gets index
+            for(int i = 0; i < schollen.Length; i++)
+            {
+                if(schollen[i].name.Equals(scholleObj))
+                {
+                    // gets position of the object (at Index i) for end of Jump
+                    endJump = schollenPosition[i];
+                }
+            }
+
+            // starts timerToAnswer to start jump
+            jump = true;           
+
+            //Debug.Log("Wir können gleich springen");
+        } else
+        {
+            // sets string of current object and starts begin for timer
+            canvasObj += correct_Name;
+            correctAnswer = false;
+        }
+
+        // sets color of the answer to green
+        GameObject.Find(canvasObj).GetComponentInChildren<Text>().color = Color.green;
+    }
+
+    public void gravityOff()
+    {
+        // sets timer only back if Schollen are not falling
+        if(schollen[0].GetComponent<Rigidbody>().isKinematic)
+        {
+            correctAnswer = true;
+            jump = false;
+            timer = timerToAnswer = 0.0f;
+            GameObject.Find(canvasObj).GetComponentInChildren<Text>().color = new Color(206, 206, 206) ;
+            canvasObj = "Canvas";
+            scholleObj = "Scholle";
+        }
+    }
+}
